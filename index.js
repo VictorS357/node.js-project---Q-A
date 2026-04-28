83301b368544f24cd60fd4a1ea755cafe98b7459
Victor357 <victorfransousa357@gmail.com>
2026-04-28 19:06:07 -0300

    node.js with express setup, ejs setup and bootstrap setup

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+//Estou dizendo para o Express usar o EJS como view engine
+//Criar uma pasta "views" e salvar os arquivos HTML dentro dela
+//Isso é um padrão que o EJS tem para a leitura dos arquivos
+app.set('view engine', 'ejs');
+app.use(express.static('public')); //aceitar carregamento de arquivos estáticos (css, js no frontend, imagens...)
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(3000, () => {
+  console.log('App rodando!');
+});
