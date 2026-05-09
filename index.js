83a51f173646d2afa6a08b107eff37e62b2edea8
VictorS357 <victorfransousa357@gmail.com>
2026-05-09 12:40:43 -0300

    install and get data from HTML to backend using body parser

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 //Estou dizendo para o Express usar o EJS como view engine
 //Criar uma pasta "views" e salvar os arquivos HTML dentro dela
 //Isso é um padrão que o EJS tem para a leitura dos arquivos
 app.set('view engine', 'ejs');
 app.use(express.static('public')); //aceitar carregamento de arquivos estáticos (css, js no frontend, imagens...)
-
+// Body parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+// Rotas
 app.get('/', (req, res) => {
   res.render('index');
 });
@@ -16,7 +20,10 @@ app.get('/perguntar', (req, res) => {
 });
 
 app.post('/salvarpergunta', (req, res) => {
-  res.send('Formulário recebido!');
+  const titulo = req.body.titulo; //Body parser permite utilizar o objeto body para pegar os dados da página pelo atributo 'name' no html
+  const descricao = req.body.descricao;
+
+  res.send(`Formulário recebido! Titulo: ${titulo} Descricao: ${descricao}`);
 });
 
 app.listen(4000, () => {
